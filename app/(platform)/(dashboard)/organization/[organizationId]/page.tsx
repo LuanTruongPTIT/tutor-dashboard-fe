8a147199b071620d48b25c700945bcead420fa30
Your Name <you@example.com>
2024-04-06 20:31:00 +0700

    update

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,25 @@
-import { auth } from "@clerk/nextjs";
-const OrganizationIdPage = () => {
-  const { userId, orgId } = auth();
-  console.log(userId, orgId);
-  return <div>xin chao</div>;
+import { Suspense } from "react";
+
+import { Separator } from "@/components/ui/separator";
+
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+// import { checkSubscription } from "@/lib/subscription";
+
+const OrganizationIdPage = async () => {
+  // const isPro = await checkSubscription();
+
+  return (
+    <div className="w-full mb-20">
+      <Info isPro={true} />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
+
 export default OrganizationIdPage;
