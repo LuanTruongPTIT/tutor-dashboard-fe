4919ce378997e6e89f69ea2a1829ea4c148cdb93
Your Name <you@example.com>
2024-05-10 08:13:49 +0700

    add feature login

---
+++ apiRequests/auth.ts
@@ -0,0 +1,21 @@
+import http from "@/lib/http";
+import { LoginBodyType } from "@/schema/auth";
+import { infer, z } from "zod";
+
+const TypeResponseLogin = z.object({
+  accessToken: z.string(),
+  refreshToken: z.string(),
+  expiresInAccessToken: z.number(),
+  expiresInRefreshToken: z.number(),
+  role: z.string(),
+  user: z.object({
+    id: z.string(),
+    email: z.string(),
+    name: z.string(),
+  }),
+});
+export type TypeResponseLogin = z.infer<typeof TypeResponseLogin>;
+export const authApiRequests = {
+  login: (body: LoginBodyType) =>
+    http.post<TypeResponseLogin>("api/auth/sign-in/tutor", body),
+};
